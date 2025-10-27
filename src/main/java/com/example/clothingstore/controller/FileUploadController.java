2f6b84dacd94d1e9928bb73e8e123a87c6f1a071
Tuấn Khang <nguyenhuutuankhang412@gmail.com>
2025-10-27 15:11:55 +0700

    feat(FileUpload): add endpoint upload Image #23

---
+++ src/main/java/com/example/clothingstore/controller/FileUploadController.java
@@ -0,0 +1,37 @@
+package com.example.clothingstore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.clothingstore.dto.fileupload.FileUploadResponse;
+import com.example.clothingstore.service.FileUploadService;
+import com.example.clothingstore.util.ApiResponse;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("file-upload")
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    // Consumer để nói kiểu gửi lên
+    @PostMapping(value = "/image", consumes = "multipart/form-data")
+    public ResponseEntity<ApiResponse<FileUploadResponse>> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            FileUploadResponse fileUploadResponse = fileUploadService.uploadImage(file);
+            return ResponseEntity.ok(new ApiResponse<>(true, null, fileUploadResponse));
+        } catch (IOException e) {
+            return ResponseEntity.status(500)
+                    .body(new ApiResponse<>(false, "Unable to save file due to I/O error:" + e.getMessage(), null));
+        }
+    }
+
+}
